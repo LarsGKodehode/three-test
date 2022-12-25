e85bcf804c629c6131fcd406b38ec3aea6939b4e
LarsG <lars.ga.solheim@gmail.com>
2022-12-25 15:25:22 +0100

    fix/ util color

--- src/utilities/color.ts
+++ src/utilities/color.ts
@@ -1,14 +1,40 @@
-type RGBA = [number, number, number, number];
+type RGBA = [
+  number,
+  number,
+  number,
+  number,
+]
 
-type RGB = [number, number, number];
+type RGB = [
+  number,
+  number,
+  number,
+]
 
-type COLOR = RGB | RGBA;
+type COLOR = RGB | RGBA
+type ColorSpec = 'rgb' | 'rgba'
 
 /**
  * Returns a color value
  */
-function getColor(): COLOR {
-  return [200, 2, 3];
+function getColor(colorSpecification: ColorSpec): COLOR {
+  switch (colorSpecification) {
+    case 'rgb':
+      return [
+        200,
+        2,
+        3,
+      ] as RGB
+      break;
+    case 'rgba':
+      return [
+        200,
+        2,
+        3,
+        1,
+      ] as RGBA
+  }
+  
 }
 
 export default getColor;
